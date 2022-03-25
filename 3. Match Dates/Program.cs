7fcb588d04c27e13e75a60d87c3b12cb3bebf72a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 11:54:07 +0200

    Task 3 - Matching dates Implementing Regex

---
+++ 3. Match Dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\d{2})(\.|\-|\/)(?<month>[A-Z]{1}[a-z]{1,2})\2(?<year>\d{4})";
+        }
+    }
+}
