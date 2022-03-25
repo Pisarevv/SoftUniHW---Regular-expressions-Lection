10d17117837daf85627079de50e2b0b4d133c696
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 11:34:32 +0200

    Adding task 2 - Validating phone number Implement Regex

---
+++ 2. Match Phone Number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |\-)2\1\d{3}\1\d{4}\b";
+        }
+    }
+}
