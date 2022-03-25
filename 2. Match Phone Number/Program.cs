7beadd38fb79f94408e203cff9309e9c29ee2666
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 11:44:40 +0200

    Final update task 2

--- 2. Match Phone Number/Program.cs
+++ 2. Match Phone Number/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _2._Match_Phone_Number
 {
@@ -7,6 +10,12 @@ namespace _2._Match_Phone_Number
         static void Main(string[] args)
         {
             string pattern = @"\+359( |\-)2\1\d{3}\1\d{4}\b";
+            string phoneNumbers = Console.ReadLine();
+
+            MatchCollection validPhoneNumbers = Regex.Matches(phoneNumbers, pattern);
+            
+
+            Console.Write(string.Join(", ", validPhoneNumbers));
         }
     }
 }
