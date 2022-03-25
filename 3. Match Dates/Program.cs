7be4a6286df4a36c52c6b76a7711a189d87b726b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-03-25 12:01:13 +0200

    Final update task 3

--- 3. Match Dates/Program.cs
+++ 3. Match Dates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _3._Match_Dates
 {
@@ -6,7 +7,20 @@ namespace _3._Match_Dates
     {
         static void Main(string[] args)
         {
-            string pattern = @"(?<day>\d{2})(\.|\-|\/)(?<month>[A-Z]{1}[a-z]{1,2})\2(?<year>\d{4})";
+            string pattern = @"(\b(?<day>\d{2})(\.|\-|\/)(?<month>[A-Z]{1}[a-z]{1,2})\2(?<year>\d{4})\b)";
+            string inputDates = Console.ReadLine();
+
+            MatchCollection validDates = Regex.Matches(inputDates, pattern);
+
+            foreach (Match match in validDates)
+            {
+                string day = match.Groups["day"].Value;
+                string month = match.Groups["month"].Value;
+                string year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
         }
     }
 }
